682f8f1eaf6f05394d4f0f11e5d01598ed570b94
farid <faridfaiz71@gmail.com>
2024-02-12 21:40:00 +0200

    main1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SliderComponent } from './slider/slider.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { Section1Component } from './section1/section1.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SliderComponent,
+    Section1Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule
+  ],
+  exports: [
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
